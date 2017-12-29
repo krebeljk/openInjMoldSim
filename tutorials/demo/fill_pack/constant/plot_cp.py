7e4ca94199e1f45610fe11a8a2666ab39a88427e
Kristjan <krebeljk@gmail.com>
2017-12-29 17:41:44 +0100

    python plot scripts to demo case: dt, iters, cp, kappa

---
+++ tutorials/demo/fill_pack/constant/plot_cp.py
@@ -0,0 +1,22 @@
+import numpy as np
+from io import StringIO
+import matplotlib.pyplot as plt
+
+# get data
+f = open('cpTable')
+bes = f.read()
+f.close()
+pod = bes.replace("("," ").replace(")", " ")
+tabela = np.loadtxt(StringIO(pod), usecols=(0,2))
+temp = np.add(tabela[:,0], -273.15)
+cp = tabela[:,1]
+
+# plot
+fig, ax = plt.subplots()
+ax.plot(temp, cp)
+ax.set(xlabel='T (degC)', ylabel='c_p (J/kgK)',
+       title='Specific heat at constant pressure')
+ax.grid()
+
+plt.show()
+# fig.savefig("cp.png")
