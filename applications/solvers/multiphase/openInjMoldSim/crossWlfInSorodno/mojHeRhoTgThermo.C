56ca72cb039cb3462b4ca7e07b0a7055461003f1
Kristjan <krebeljk@gmail.com>
2018-11-24 13:44:13 +0100

    ../crossWlfInSorodno/mojHeRhoTgThermo rename vf to v (spec. volume)

--- applications/solvers/multiphase/openInjMoldSim/crossWlfInSorodno/mojHeRhoTgThermo.C
+++ applications/solvers/multiphase/openInjMoldSim/crossWlfInSorodno/mojHeRhoTgThermo.C
@@ -40,7 +40,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
     scalarField& rhoCells = this->rho_.internalField();
     scalarField& muCells = this->mu_.internalField();
     scalarField& alphaCells = this->alpha_.internalField();
-    scalarField& vfCells = this->vf_.internalField();
+    scalarField& vCells = this->v_.internalField();
 
     forAll(TCells, celli)
     {
@@ -60,7 +60,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
         muCells[celli] = mixture_.mu(pCells[celli], TCells[celli], strigCells[celli]);
 
         alphaCells[celli] = mixture_.alphah(pCells[celli], TCells[celli]);
-        vfCells[celli] =   mixture_.veq(pCells[celli], TCells[celli])
+        vCells[celli] =   mixture_.veq(pCells[celli], TCells[celli])
                          - mixture_.vg(pCells[celli], TCells[celli]);
     }
 
@@ -79,7 +79,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
 
         fvPatchScalarField& pmu = this->mu_.boundaryField()[patchi];
         fvPatchScalarField& palpha = this->alpha_.boundaryField()[patchi];
-        fvPatchScalarField& pvf = this->vf_.boundaryField()[patchi];
+        fvPatchScalarField& pv = this->v_.boundaryField()[patchi];
 
         if (pT.fixesValue())
         {
@@ -95,7 +95,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
                 prho[facei] = mixture_.rho(pp[facei], pT[facei]);
                 pmu[facei] = mixture_.mu(pp[facei], pT[facei], pstrig[facei]);
                 palpha[facei] = mixture_.alphah(pp[facei], pT[facei]);
-                pvf[facei] =    mixture_.veq(pp[facei], pT[facei])
+                pv[facei] =    mixture_.veq(pp[facei], pT[facei])
                               - mixture_.vg(pp[facei], pT[facei]);
             }
         }
@@ -112,7 +112,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
                 prho[facei] = mixture_.rho(pp[facei], pT[facei]);
                 pmu[facei] = mixture_.mu(pp[facei], pT[facei], pstrig[facei]);
                 palpha[facei] = mixture_.alphah(pp[facei], pT[facei]);
-                pvf[facei] =    mixture_.veq(pp[facei], pT[facei])
+                pv[facei] =    mixture_.veq(pp[facei], pT[facei])
                               - mixture_.vg(pp[facei], pT[facei]);
             }
         }
@@ -130,18 +130,18 @@ Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::mojHeRhoTgThermo
 )
 :
     mojHeThermo<BasicPsiThermo, MixtureType>(mesh, phaseName),
-    vf_
+    v_
     (
         IOobject
         (
-            "vf",
+            "v",
             mesh.time().timeName(),
             mesh,
             IOobject::NO_READ,
             IOobject::AUTO_WRITE
         ),
         mesh,
-        dimensionedScalar("vfZero", dimensionSet(1,-3,0,0,0,0,0), 0.0),
+        dimensionedScalar("vZero", dimensionSet(1,-3,0,0,0,0,0), 0.0),
         "zeroGradient"
     )
 {
