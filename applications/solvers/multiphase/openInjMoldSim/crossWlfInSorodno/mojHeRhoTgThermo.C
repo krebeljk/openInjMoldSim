4caaa4bd7247adf85d4519b35f3a1dce46616d61
Kristjan <krebeljk@gmail.com>
2018-11-25 13:11:21 +0100

    ../crossWlfInSorodno/mojHeRhoTgThermo.C: calc tauRlx

--- applications/solvers/multiphase/openInjMoldSim/crossWlfInSorodno/mojHeRhoTgThermo.C
+++ applications/solvers/multiphase/openInjMoldSim/crossWlfInSorodno/mojHeRhoTgThermo.C
@@ -57,6 +57,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
     scalarField& vgCells = vg.internalField();
     scalarField& muCells = this->mu_.internalField();
     scalarField& alphaCells = this->alpha_.internalField();
+    scalarField& sfTauRlx = tauRlx.internalField();
 
     forAll(TCells, celli)
     {
@@ -77,6 +78,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
         muCells[celli] = mixture_.mu(pCells[celli], TCells[celli], strigCells[celli]);
 
         alphaCells[celli] = mixture_.alphah(pCells[celli], TCells[celli]);
+        sfTauRlx[celli] = mixture_.tauRlx(pCells[celli], TCells[celli]);
     }
 
     forAll(this->T_.boundaryField(), patchi)
@@ -95,6 +97,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
 
         fvPatchScalarField& pmu = this->mu_.boundaryField()[patchi];
         fvPatchScalarField& palpha = this->alpha_.boundaryField()[patchi];
+        fvPatchScalarField& ptauRlx = tauRlx.boundaryField()[patchi];
 
         if (pT.fixesValue())
         {
@@ -111,6 +114,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
                 pvg[facei] = mixture_.vg(pp[facei], pT[facei]);
                 pmu[facei] = mixture_.mu(pp[facei], pT[facei], pstrig[facei]);
                 palpha[facei] = mixture_.alphah(pp[facei], pT[facei]);
+                ptauRlx[facei] = mixture_.tauRlx(pp[facei], pT[facei]);
             }
         }
         else
@@ -127,6 +131,7 @@ void Foam::mojHeRhoTgThermo<BasicPsiThermo, MixtureType>::calculate()
                 pvg[facei] = mixture_.vg(pp[facei], pT[facei]);
                 pmu[facei] = mixture_.mu(pp[facei], pT[facei], pstrig[facei]);
                 palpha[facei] = mixture_.alphah(pp[facei], pT[facei]);
+                ptauRlx[facei] = mixture_.tauRlx(pp[facei], pT[facei]);
             }
         }
     }
